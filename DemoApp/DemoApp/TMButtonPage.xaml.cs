6bc1730277a2ded9c7e6515cd6c094d9cdfc9fe1
ElishaSamPeterPrabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2023-05-26 17:30:02 +0530

    Fix/TMbutton (#76)
    * Button
    
    Remove Color Properties for Background Text and BorderRadius
    Add ButtonStyle ButtonColor
    Modify Floating Button which now only has radius set for pill shaped view
    
    * Button
    
    Modify Demo App on how buttons appear
    
    * Button
    
    Modify DemoPage
    Modify OnClick Color and IsDisabled Opacity
    
    * Button
    
    Modify Setting Color when clicked
    Rename to galley_icon
    Reduce Floating button shadow
    
    * Button
    
    Modify Number of taps required
    
    * Button
    
    Remove None From Enums ButtonColor, ButtonStyle

--- DemoApp/DemoApp/TMButtonPage.xaml.cs
+++ DemoApp/DemoApp/TMButtonPage.xaml.cs
@@ -1,16 +1,99 @@
+using Trimble.Modus.Components;
+using Trimble.Modus.Components.Enums;
+using Size = Trimble.Modus.Components.Enums.Size;
+
 namespace DemoApp;
 
 public partial class TMButtonPage : ContentPage
 {
-	public TMButtonPage()
-	{
-		InitializeComponent();
-	}
+    private string _selectedStyle ="Fill";
+    private string _selectedSize ="Default";
+    private bool _isDiabled;
 
-    private void button_Clicked(object sender, EventArgs e)
+    public bool IsDisabled
     {
-        Console.WriteLine("Clicked");
+        get => _isDiabled;
+        set
+        {
+            if (_isDiabled != value)
+            {
+                _isDiabled = value;
+                ValidateRadioButtons();
+            }
+        }
     }
 
- 
+
+    public TMButtonPage()
+	{
+        
+		InitializeComponent();
+    }
+
+    private void Style_Changed(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is RadioButton radioButton && radioButton.IsChecked)
+        {
+            _selectedStyle = radioButton.Value.ToString();
+            ValidateRadioButtons();
+        }
+    }
+
+    private void Size_Changed(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is RadioButton radioButton && radioButton.IsChecked)
+        {
+            _selectedSize = radioButton.Value.ToString();
+            ValidateRadioButtons();
+        }
+    }
+
+    private void ValidateRadioButtons()
+    {
+        bool isStyleSelected = !string.IsNullOrEmpty(_selectedStyle);
+        bool isSizeSelected = !string.IsNullOrEmpty(_selectedSize);
+
+        if (isStyleSelected && isSizeSelected)
+        {
+            List<TMButton> buttons = new List<TMButton> { button1, button2, button3, button4 ,button1i ,button2i ,button3i ,button4i };
+            List<TMButton> iconbuttons = new List<TMButton> { button1i, button2i, button3i, button4i };
+
+           
+            foreach (TMButton button in buttons)
+            {
+              
+                button.Size = (Size)Enum.Parse(typeof(Size), _selectedSize);
+                button.ButtonStyle = (ButtonStyle)Enum.Parse(typeof(ButtonStyle), _selectedStyle);
+                int index = buttons.IndexOf(button);
+                if (index >= buttons.Count - 4)
+                {
+                    if (button.ButtonStyle == ButtonStyle.Outline || button.ButtonStyle == ButtonStyle.BorderLess)
+                    {
+                        button.IconSource = ImageSource.FromFile("icondark.png");
+                    }
+                    else
+                    {
+                        if (!(button.ButtonColor == ButtonColor.Tertiary))
+                        {
+                            button.IconSource = ImageSource.FromFile("gallery_icon.png");
+                        }
+                    }
+                }
+
+                if (_isDiabled)
+                {
+                    button.IsDisabled = true;
+                }
+                else
+                {
+                    button.IsDisabled = false;
+                }
+            }
+        }
+    }
+
+    private void isDisabled_Toggled(object sender, ToggledEventArgs e)
+    {
+        IsDisabled = e.Value;
+    }
 }
