bc08783ff86c8072587adc55a31d6d805fc6913d
Balasubramanian R <130034598+BAL4-R@users.noreply.github.com>
2023-06-12 16:31:16 +0530

    Add Radio button and Radio button group (#123)
    * Create radio button and group
    
    * Add wrap layout
    Add itemsource property for group
    Checkbox icon changed
    
    * Use IEnumerable for ItemSource.
    Added missing regions and code formatted.
    Remove ObservableCollectionExtension
    
    * Update selected radio button finding method

--- Trimble.Modus.Components/Constant/ImageConstants.cs
+++ Trimble.Modus.Components/Constant/ImageConstants.cs
@@ -1,8 +1,16 @@
-﻿namespace Trimble.Modus.Components.Contant
+namespace Trimble.Modus.Components.Contant
 {
     internal static class ImageConstants
     {
         //TODO: Add other images here
         public const string CloseButtonImage = "modus_close_icon.png";
+
+        public const string DefaultRadioButton = "radio_unselected_icon.png";
+
+        public const string SelectedRadioButton = "radio_selected_icon.png";
+
+        public const string IndeterminateCheckBoxButton = "indeterminate_checkbox.png";
+        public const string DefaultCheckBoxButton = "default_checkbox.png";
+        public const string CheckedCheckBoxButton = "checked_checkbox.png";
     }
 }
