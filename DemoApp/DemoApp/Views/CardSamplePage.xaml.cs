0ea3df99124bf90d9251dae0d987aded5ac5ebcc
Sofiya K <Sofiya_k@trimble.com>
2023-12-01 17:05:57 +0530

    Changes for selected event

--- DemoApp/DemoApp/Views/CardSamplePage.xaml.cs
+++ DemoApp/DemoApp/Views/CardSamplePage.xaml.cs
@@ -17,18 +17,15 @@ public partial class CardSamplePage : ContentPage
     {
         Console.WriteLine("Execute " + e);
     }
-
-    private void IsSelectedToggled(object sender, ToggledEventArgs e)
+    private void IsSelectedToggled(object sender, TMSwitchEventArgs e)
     {
         List<TMCard> cardlist = new List<TMCard>() { card1, card2, card3, card4, card5, card6 };
-        foreach (TMCard card in cardlist)
-        {
-            card.IsSelected = e.Value;
-        }
+        cardlist.ForEach(x => x.IsSelected = e.Value);
     }
-
     private void Card1Clicked(object sender, EventArgs e)
     {
         Console.WriteLine("Card1 Clicked");
     }
+
+
 }
