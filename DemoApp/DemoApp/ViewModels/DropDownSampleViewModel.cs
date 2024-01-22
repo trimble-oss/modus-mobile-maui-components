87c9a42c548567b6ca8ad7eeb38e47b54a1d1be6
pooja-ps-trimble <148334972+pooja-ps-trimble@users.noreply.github.com>
2024-01-22 09:27:50 +0530

    Added SelectedIndexChanged Event for DropDown

--- DemoApp/DemoApp/ViewModels/DropDownSampleViewModel.cs
+++ DemoApp/DemoApp/ViewModels/DropDownSampleViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trimble.Modus.Components;
+using DropDownSelectionChangedEventArgs = Trimble.Modus.Components.DropDownSelectionChangedEventArgs;
+
 
 namespace DemoApp.ViewModels
 {
@@ -20,6 +24,17 @@ namespace DemoApp.ViewModels
             DropdownSource = new List<string>() { "Excavator", "Bulldozer", "Loader", "Grader", "Trencher", "Backhoe", "Compactors", "Crane" };
             ColorSource = new List<string>() { "Red", "Yellow", "Black" };
             SelectedIndex = 6; ;
+        }
+        [RelayCommand]
+        private void ItemSelected(DropDownSelectionChangedEventArgs e)
+        {
+            if (e.PreviousSelection == null)
+            {
+                Console.WriteLine("PreviousSelection is null");
+            }
+            else
+                Console.WriteLine("PreviousSelection " + e.PreviousSelection.ToString());
+            Console.WriteLine("CurrentSelection " + e.CurrentSelection.ToString());
         }
     }
 }
