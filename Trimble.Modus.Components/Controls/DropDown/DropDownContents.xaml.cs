32f385b6f58bf921ec44f421652ce828f41d69c2
Sofiya K <Sofiya_k@trimble.com>
2024-07-19 11:01:35 +0530

    Color Customistation for drop down

--- Trimble.Modus.Components/Controls/DropDown/DropDownContents.xaml.cs
+++ Trimble.Modus.Components/Controls/DropDown/DropDownContents.xaml.cs
@@ -5,6 +5,25 @@ namespace Trimble.Modus.Components;
 
 public partial class DropDownContents : PopupPage
 {
+    public ListView dropDownListView;
+    public static readonly BindableProperty BorderColorProperty =
+    BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(DropDownContents), Colors.Transparent, BindingMode.Default, null, propertyChanged: OnBorderColorPropertyChanged);
+
+    public static readonly BindableProperty SelectedItemProperty =
+      BindableProperty.Create(nameof(SelectedItem), typeof(object), typeof(DropDownContents), propertyChanged: OnSelectedItemChanged);
+
+    internal Color BorderColor
+    {
+        get => (Color)GetValue(BorderColorProperty);
+        set => SetValue(BorderColorProperty, value);
+    }
+
+    public object SelectedItem
+    {
+        get => GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
     internal DropDownContents()
     {
 
@@ -13,6 +32,8 @@ public partial class DropDownContents : PopupPage
     internal DropDownContents(View anchorView, Enums.ModalPosition position) : base(anchorView, position)
     {
         InitializeComponent();
+        dropDownListView = listView;
+        this.SetDynamicResource(StyleProperty, "DefaulDropDownContentstStyle");
     }
 
     public Thickness Margin { get; set; }
@@ -38,6 +59,7 @@ public partial class DropDownContents : PopupPage
         {
             listView.SelectedItem = ItemSource?.Cast<object>()?.ToList()[SelectedIndex];
         }
+        UpdateBackgroundColorOfCell(listView);
         listView.ItemSelected += SelectedEventHandler;
         if (Height - YPosition < DesiredHeight)
         {
@@ -46,6 +68,43 @@ public partial class DropDownContents : PopupPage
 #if WINDOWS
             border.Margin = new Thickness(0, 24, 10, 0);
 #endif
+        }
+    }
+
+    private static void UpdateBackgroundColorOfCell(ListView listView)
+    {
+        if (listView.SelectedItem != null)
+        {
+            foreach (var item in listView.TemplatedItems)
+            {
+                if (item is DropDownViewCell textCell)
+                {
+                    if (listView.SelectedItem == textCell.BindingContext)
+                    {
+                        textCell.UpdateHighlightBackgroundColor();
+                    }
+                    else
+                    {
+                        textCell.UpdateDefaultBackgroundColor();
+                    }
+                }
+            }
+        }
+    }
+
+    private static void OnSelectedItemChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is DropDownContents dropDown)
+        {
+            dropDown.listView.SelectedItem = newValue;
+        }
+    }
+
+    private static void OnBorderColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is DropDownContents dropDown)
+        {
+            dropDown.border.Stroke = (Color)newValue;
         }
     }
 }
