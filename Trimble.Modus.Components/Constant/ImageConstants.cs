5ad290df6ea5ba3aa05d6aeeaf185686fd8bcf31
Sofiya K <Sofiya_k@trimble.com>
2024-07-23 18:25:47 +0530

    Removed tint color for dropdown icon

--- Trimble.Modus.Components/Constant/ImageConstants.cs
+++ Trimble.Modus.Components/Constant/ImageConstants.cs
@@ -30,6 +30,7 @@ namespace Trimble.Modus.Components.Constant
         public const string Check = "check.png";
         public const string CancelCircle = "cancel_circle.png";
         public const string ChevronDownIcon = "chevron_down_icon.png";
+        public const string ChevronDownIconWhite = "chevron_down_icon_white.png";
         public const string SliderThumbIcon = "slider_thumb.png";
     }
 }
--- Trimble.Modus.Components/Controls/DropDown/TMDropDown.xaml.cs
+++ Trimble.Modus.Components/Controls/DropDown/TMDropDown.xaml.cs
@@ -117,8 +117,6 @@ public partial class TMDropDown : ContentView
         if (bindable is TMDropDown dropDown)
         {
             dropDown.label.TextColor = (Color)newValue;
-            dropDown.indicatorButton.Behaviors.Clear();
-            dropDown.indicatorButton.Behaviors.Add(new IconTintColorBehavior { TintColor = (Color)newValue });
         }
     }
 
